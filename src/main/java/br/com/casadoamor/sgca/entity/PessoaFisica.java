744f91030abe2abf8811748890d4acbe629b686c
Tácio Patez <taciopatez@hotmail.com>
2025-07-10 19:08:08 -0300

    Correção Numero Digitos RG

--- src/main/java/br/com/casadoamor/sgca/entity/PessoaFisica.java
+++ src/main/java/br/com/casadoamor/sgca/entity/PessoaFisica.java
@@ -6,23 +6,23 @@ import lombok.*;
 
 import java.util.Date;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "pessoa_fisica")
+
 public class PessoaFisica {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+    @Column(nullable = false)@Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Entity
+    @Table(name = "pessoa_fisica")
     private String nome;
     private Date dataNascimento;
     @Column(unique = true, nullable = false, length = 11)
     private String cpf;
-    @Column(unique = true, length = 9)
+    @Column(unique = true, length = 10)
     private String rg;
     private String naturalidade;
     private String profissao;
